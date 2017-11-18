887aeded03feb184270adb6e9a6081b84863dc85
Meet Zaveri <meethpatel96@gmail.com>
2017-11-18 19:46:22 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import TaskList from './tasklist.js';
+import TaskList from './tasklist.js'; // importing created custom components 
 import Date from './date.js';
 import Avatar from './avatar.js';
 import AddButton from './addbuttons.js';
@@ -12,34 +12,34 @@ class App extends Component {
     this.state = {
       tasks= [
         {
-        'time':'11',
-        'period':'am', 
-        'activity_title':'Calling Off',
-        'activity_description':'#reactforenewbies'
+          'time':'11',
+          'period':'am', 
+          'activity_title':'Calling Off',
+          'activity_description':'#reactforenewbies'
         },
         {
-        'time':'11',
-        'period':'am', 
-        'activity_title':'Meeting with Team Leads',
-        'activity_description':'New Project Kickoff'
+          'time':'11',
+          'period':'am', 
+          'activity_title':'Meeting with Team Leads',
+          'activity_description':'New Project Kickoff'
         },
         {
-        'time':'11',
-        'period':'am', 
-        'activity_title':'Call Mom',
-        'activity_description':'Return her call before she kills me'
+          'time':'11',
+          'period':'am', 
+          'activity_title':'Call Mom',
+          'activity_description':'Return her call before she kills me'
         },
         {
-        'time':'11',
-        'period':'am', 
-        'activity_title':'Fix Wifeys website',
-        'activity_description':'FB Ads Integration not working'
+          'time':'11',
+          'period':'am', 
+          'activity_title':'Fix Wifeys website',
+          'activity_description':'FB Ads Integration not working'
         },
         {
-        'time':'11',
-        'period':'am', 
-        'activity_title':'Do DB Backups',
-        'activity_description':'Related to upcoming server migration'
+          'time':'11',
+          'period':'am', 
+          'activity_title':'Do DB Backups',
+          'activity_description':'Related to upcoming server migration'
         },
       ]
     }
@@ -47,12 +47,12 @@ class App extends Component {
   render() {
     return (
       <div style={{padding: '30px 30px'}}>
-          <Avatar />
-          <br />
-          <Date />
-          <br />
-          <TaskList tasks={this.state.tasks}/>
-          <br />
+            <Avatar />
+              <br />
+            <Date />
+              <br />
+            <TaskList tasks={this.state.tasks}/>
+              <br />
           <AddButton />
       </div>
     );
